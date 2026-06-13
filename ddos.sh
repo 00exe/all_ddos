017acc76211391357be993611764d9d60e5e05f6
00exe <exe9469@gmail.com>
2026-06-13 19:49:06 +0300

    ddos.sh

--- ddos.sh
+++ ddos.sh
@@ -13,12 +13,4 @@ case $sc in
      clear
      python3 all-ddos.py
      ;;
-  3)
-     cd DDoS-Ripper
-     clear
-     read -p "IP:" ip
-     read -p "PORT:" port
-     read -p "TURBO:" trb
-     python3 DRipper.py -s $ip -p $port -t $trb
-     ;;
 esac
